a50e7a28dc83826ea88a49c0a687923f33f3b2f2
collintegral <collintegral@gmail.com>
2024-01-22 18:25:48 -0500

    Prepare w03 work.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fract1 = new Fraction();
+        Console.WriteLine(fract1.getFractionString());
+        Console.WriteLine(fract1.getDecimalValue());
+        Fraction fract2 = new Fraction(5);
+        Console.WriteLine(fract2.getFractionString());
+        Console.WriteLine(fract2.getDecimalValue());
+        Fraction fract3 = new Fraction(3,4);
+        Console.WriteLine(fract3.getFractionString());
+        Console.WriteLine(fract3.getDecimalValue());
+        Fraction fract4 = new Fraction(1,3);
+        Console.WriteLine(fract4.getFractionString());
+        Console.WriteLine(fract4.getDecimalValue());
     }
 }
