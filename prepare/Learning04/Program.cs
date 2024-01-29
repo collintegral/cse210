da60b9ec18da5acfcba72877ab892293ec203c6f
collintegral <collintegral@gmail.com>
2024-01-29 18:37:28 -0500

    Learn activity w04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment sam = new("Samuel Bennet", "Multiplication");
+        Console.WriteLine(sam.GetSummary());
+
+        MathAssignment rob = new("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        Console.WriteLine(rob.GetSummary());
+        Console.WriteLine(rob.GetHomeworkList());
+
+        WritingAssignment mary = new("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(mary.GetSummary());
+        Console.WriteLine(mary.GetWritingInformation());
     }
 }
