7be682535f2035606e1614190f8cc28277f22030
collintegral <collintegral@gmail.com>
2024-02-05 18:29:15 -0500

    Completed prepare w05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 10, 4));
+        shapes.Add(new Circle("Green", 6));
+
+        shapes.ForEach(shape => {
+            Console.WriteLine($"{shape.GetColor()} {shape.GetType()} Area: {shape.GetArea()}");
+        });
     }
 }
